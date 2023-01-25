3851bb04ad6a17d804ce5dae928aeb93d231b0b0
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2023-01-25 21:31:20 +0200

    Up

--- src/components/servisApi.js
+++ src/components/servisApi.js
@@ -5,5 +5,5 @@ const KEY =  '32850209-97f2951747f8bc30e5bbd4a42';
 
 export const fetchImages = async (name, page) => {
     const response = await axios.get(`${BASE_URL}?q=${name}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`);
-    return response.data;
+    return response;
     };
